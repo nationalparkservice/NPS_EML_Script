9d2db19c2ebbd5271bba6be0fb09bcc5ff18c9f3
juddpatterson <judd_patterson@nps.gov>
2022-10-07 09:53:53 -0600

    updating to 120 column width

--- EML_Creation_Script.R
+++ EML_Creation_Script.R
@@ -1,34 +1,37 @@
-# Script Overview---------------------------------------------------------------
+# Script Overview-------------------------------------------------------------------------------------------------------
 # Title: NPS EML Creation Script
 #
-# Summary: This code creates an EML file for a data package by leveraging several functions within the EMLassemblyline packages. In this case
-# the example inputs are for a EVER Veg Map AA dataset and are meant to either be run as a test of the process or to be replaced with your own
-# content. This is a step by step process where each section (indicated by #####) should be reviewed and edited if necessary,and run one by one. 
-# Several sections are labeled as OPTIONAL and may only apply to certain data packages. The final section has the make_eml() function to put
-# together a validated EML metadata file. Future updates to this script will help bring in additional functions from the EMLeditor (part of the
+# Summary: This code creates an EML file for a data package by leveraging several functions within the EMLassemblyline
+# package. In this case the example inputs are for a EVER Veg Map AA dataset and are meant to either be run as a test
+# of the process or to be replaced with your own content. This is a step by step process where each section (indicated
+# by #####) should be reviewed and edited if necessary,and run one by one. Several sections are labeled as OPTIONAL and
+# may only apply to certain data packages. The final section has the make_eml() function to put together a validated EML
+# metadata file. Future updates to this script will help bring in additional functions from the EMLeditor (part of the
 # NPSdataverse) that are used to populate NPS DataStore specific tags.
 
-# Created By: Judd Patterson (judd_patterson@nps.gov)
-# Last Updated: October 7, 2022 (robert_baker@nps.gov)
+# Contributors: Judd Patterson (judd_patterson@nps.gov) and Rob Baker (robert_baker@nps.gov)
+# Last Updated: October 7, 2022
 
-# Install and Load Packages-----------------------------------------------------
-# Uncomment the next three lines if you've never installed EMLassemblyline before
+# Install and Load R Packages-------------------------------------------------------------------------------------------
+# Install packages - uncomment the next three lines if you've never installed EMLassemblyline before
 #install.packages("devtools")
 #library(devtools)
 #install_github("EDIorg/EMLassemblyline")
+
+# Load packages
 library(EMLassemblyline)
 library(lubridate)
 library(tidyverse)
 
-# Set Overall Package Details----------------------------------------------------
-# Review and update these items. For vectors with more than one item, keep the order the same (i.e. item #1 should correspond to the same file
-# in each vector)
+# Set Overall Package Details-------------------------------------------------------------------------------------------
+# Review and update these items. For vectors with more than one item, keep the order the same (i.e. item #1 should
+# correspond to the same file in each vector)
 
-#Metadata filename - this becomes the filename, so make sure it ends in _metadata to comply with the NPS data package specifications
+# Metadata filename - this becomes the filename, so make sure it ends in _metadata to comply with data package specifications
 metadata_id <- "TEST_EVER_AA_metadata"
 
 #Overall package title (replace with our title)
-package_title <- "TEST_Title"
+package_title <- "TEST_Everglades National Park Accuracy Assessment (AA) Data Package"
 
   #Description of data collection status - choose from 'ongoing' or 'complete'
   data_type <- "complete"
